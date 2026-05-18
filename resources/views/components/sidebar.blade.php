fddfd520e917bcbba3e98d892d39a2eccc1a8edd
regacode4 <regacode4@gmail.com>
2026-05-18 20:15:49 +0700

    Refactor : Merubah susunan kode agar rapi dengan component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+            <div class="m-header">
+                <a href="../dashboard/index.html" class="b-brand text-primary">
+                    <span>Aplikasi Manajemen Pegawai</span>
+                </a>
+            </div>
+            <div class="navbar-content">
+                <ul class="pc-navbar">
+                    <x-sidebar.links title="Home" icon="ti ti-typography" route="home" />
+                    <x-sidebar.links title="Data Pegawai" icon="ti ti-users" route="pegawai.index" />
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
