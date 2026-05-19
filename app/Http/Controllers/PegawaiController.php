c56b0776ff566e1226cc241752fafd79a6849130
regacode4 <regacode4@gmail.com>
2026-05-19 20:22:02 +0700

    feat : menambahkan fitur upload foto

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Pegawai;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PegawaiController extends Controller
 {
@@ -18,17 +20,19 @@ class PegawaiController extends Controller
         return view('pegawai.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         //dd($request->all());
         $request->validate([
             'nama_pegawai' => 'required',
+            'foto' => 'required|image|mimes:jpeg,png,jpg|max:2048',
             'nik' => 'required|numeric|unique:pegawais',
             'alamat' => 'required',
             'umur' => 'required|numeric',
             'tanggal_lahir' => 'required|date',
             'tempat_lahir' => 'required',
             'jenis_kelamin' => 'required|in:Laki-laki,Perempuan',
-        ],[
+        ], [
             'nama_pegawai.required' => 'Nama pegawai harus diisi',
             'nik.required' => 'NIK harus diisi',
             'alamat.required' => 'Alamat harus diisi',
@@ -48,7 +52,15 @@ class PegawaiController extends Controller
         //     'jenis_kelamin' => $request->jenis_kelamin,
         // ]);
 
-        Pegawai::create($request->all());
+        $foto = $request->file('foto');
+        $fileName = Str::uuid() . '.' . $foto->getClientOriginalExtension();
+
+        Storage::disk('public')->putFileAs('foto_pegawai', $foto, $fileName);
+
+        $newRequest = $request->all();
+        $newRequest['foto'] = $fileName;
+
+        Pegawai::create($newRequest);
         return redirect()->route('pegawai.index')->with('success', 'Data pegawai berhasil ditambahkan');
     }
 
@@ -62,6 +74,7 @@ class PegawaiController extends Controller
     {
         $request->validate([
             'nama_pegawai' => 'required',
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
             'nik' => 'required|numeric|unique:pegawais,nik,' . $pegawai->id,
             'alamat' => 'required',
             'umur' => 'required|numeric',
@@ -78,6 +91,17 @@ class PegawaiController extends Controller
             'jenis_kelamin.required' => 'Jenis kelamin harus dipilih',
         ]);
 
+        $fileName = $pegawai->foto;
+        $foto = $request->file('foto');
+        if($foto) {
+            $fileName = Str::uuid() . '.' . $foto->getClientOriginalExtension();
+            Storage::disk('public')->putFileAs('foto_pegawai', $foto, $fileName);
+        } else {
+            $fileName = $pegawai->foto;
+        }
+
+        $newRequest = $request->except('nik');
+        $newRequest['foto'] = $fileName;
         // $pegawai->update($request->all());
 
         // menghindari injection
@@ -90,14 +114,19 @@ class PegawaiController extends Controller
         //     'jenis_kelamin' => $request->jenis_kelamin,
         // ]);
 
-        $pegawai->update($request->except('nik'));
+        $pegawai->update($newRequest);
 
         return redirect()->route('pegawai.index')->with('success', 'Data pegawai berhasil diperbarui');
     }
 
     public function destroy(String $id)
     {
-        Pegawai::destroy($id);
+        //Pegawai::destroy($id);
+        $pegawai = Pegawai::find($id);
+        if($pegawai->foto) {
+            Storage::disk('public')->delete('foto_pegawai/' . $pegawai->foto);
+        }
+        $pegawai->delete();
         return redirect()->route('pegawai.index')->with('success', 'Data pegawai berhasil dihapus');
     }
 }
--- resources/views/pegawai/create.blade.php
+++ resources/views/pegawai/create.blade.php
@@ -1,89 +1,107 @@
 @extends('layouts.mantis')
 
 @section('content')
-<div class="">
-    <div class="card">
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <h4 class="card-title">
-                Form Data Pegawai
-            </h4>
-            <div>
-                <a href="{{ route('pegawai.index')}}">Kembali</a>
+    <div class="">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4 class="card-title">
+                    Form Data Pegawai
+                </h4>
+                <div>
+                    <a href="{{ route('pegawai.index') }}">Kembali</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('pegawai.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group my-2">
+                        <label for="nama_pegawai">Nama Pegawai</label>
+                        <input type="text" name="nama_pegawai" id="nama_pegawai"
+                            class="form-control @error('nama_pegawai') 
+                        is-invalid @enderror"
+                            value="{{ old('nama_pegawai') }}" autofocus>
+                        @error('nama_pegawai')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="nik">NIK</label>
+                        <input type="text" name="nik" id="nik"
+                            class="form-control @error('nik') 
+                        is-invalid @enderror"
+                            value="{{ old('nik') }}">
+                        @error('nik')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="umur">Umur</label>
+                        <input type="number" name="umur" id="umur"
+                            class="form-control @error('umur') 
+                        is-invalid @enderror"
+                            value="{{ old('umur') }}">
+                        @error('umur')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="jenis_kelamin">Jenis Kelamin</label>
+                        <select name="jenis_kelamin" id="jenis_kelamin"
+                            class="form-control @error('jenis_kelamin') 
+                        is-invalid @enderror">
+                            <option value="">Pilih jenis kelamin</option>
+                            <option value="Laki-laki" {{ old('jenis_kelamin') == 'Laki-laki' ? 'selected' : '' }}>Laki-laki
+                            </option>
+                            <option value="Perempuan" {{ old('jenis_kelamin') == 'Perempuan' ? 'selected' : '' }}>Perempuan
+                            </option>
+                        </select>
+                        @error('jenis_kelamin')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="tanggal_lahir">Tanggal Lahir</label>
+                        <input type="date" name="tanggal_lahir" id="tanggal_lahir"
+                            class="form-control @error('tanggal_lahir') 
+                        is-invalid @enderror"
+                            value="{{ old('tanggal_lahir') }}">
+                        @error('tanggal_lahir')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="tempat_lahir">Tempat Lahir</label>
+                        <input type="text" name="tempat_lahir" id="tempat_lahir"
+                            class="form-control @error('tempat_lahir') 
+                        is-invalid @enderror"
+                            value="{{ old('tempat_lahir') }}">
+                        @error('tempat_lahir')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="foto">Foto pegawai</label>
+                        <input type="file" name="foto" id="foto"
+                            class="form-control @error('foto') 
+                        is-invalid @enderror">
+                        @error('foto')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="alamat">Alamat</label>
+                        <textarea name="alamat" id="alamat"
+                            class="form-control @error('alamat') 
+                        is-invalid @enderror">{{ old('alamat') }}</textarea>
+                        @error('alamat')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="my-2 d-flex justify-content-end ">
+                        <button class="btn btn-primary">Simpan Data</button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="card-body">
-            <form action="{{ route('pegawai.store') }}" method="POST" class="">
-                @csrf
-                <div class="form-group my-2">
-                    <label for="nama_pegawai">Nama Pegawai</label>
-                    <input type="text" name="nama_pegawai" id="nama_pegawai" class="form-control @error('nama_pegawai') 
-                        is-invalid @enderror"
-                        value="{{ old('nama_pegawai') }}" autofocus>
-                    @error('nama_pegawai')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group my-2">
-                    <label for="nik">NIK</label>
-                    <input type="text" name="nik" id="nik" class="form-control @error('nik') 
-                        is-invalid @enderror"
-                        value="{{ old('nik') }}">
-                    @error('nik')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group my-2">
-                    <label for="umur">Umur</label>
-                    <input type="number" name="umur" id="umur" class="form-control @error('umur') 
-                        is-invalid @enderror"
-                        value="{{ old('umur') }}">
-                    @error('umur')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div>
-                    <label for="jenis_kelamin">Jenis Kelamin</label>
-                    <select name="jenis_kelamin" id="jenis_kelamin" class="form-control @error('jenis_kelamin') 
-                        is-invalid @enderror">
-                        <option value="">Pilih jenis kelamin</option>
-                        <option value="Laki-laki" {{ old('jenis_kelamin') == 'Laki-laki' ? 'selected' : '' }}>Laki-laki</option>
-                        <option value="Perempuan" {{ old('jenis_kelamin') == 'Perempuan' ? 'selected' : '' }}>Perempuan</option>
-                    </select>
-                    @error('jenis_kelamin')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group my-2">
-                    <label for="tanggal_lahir">Tanggal Lahir</label>
-                    <input type="date" name="tanggal_lahir" id="tanggal_lahir" class="form-control @error('tanggal_lahir') 
-                        is-invalid @enderror"
-                        value="{{ old('tanggal_lahir') }}">
-                    @error('tanggal_lahir')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group my-2">
-                    <label for="tempat_lahir">Tempat Lahir</label>
-                    <input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control @error('tempat_lahir') 
-                        is-invalid @enderror"
-                        value="{{ old('tempat_lahir') }}">
-                    @error('tempat_lahir')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group my-2">
-                    <label for="alamat">Alamat</label>
-                    <textarea name="alamat" id="alamat" class="form-control @error('alamat') 
-                        is-invalid @enderror">{{ old('alamat') }}</textarea>
-                    @error('alamat')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="my-2 d-flex justify-content-end ">
-                    <button class="btn btn-primary">Simpan Data</button>
-                </div>
-            </form>
-        </div>
     </div>
-</div>
 @endsection
